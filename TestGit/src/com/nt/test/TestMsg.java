2200cce200a97ce252b8e89d71bb6c44f30bcafe
somdev.das14@gmail.com <somdev.das14@gmail.com>
2019-02-23 23:46:07 +0530

    created com.nt.test package and TesMsg class only for hello world

---
+++ TestGit/src/com/nt/test/TestMsg.java
@@ -0,0 +1,11 @@
+package com.nt.test;
+
+public class TestMsg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hello sir how are you");
+	}
+
+}
